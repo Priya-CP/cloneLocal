5e9a67e73888affc8ec4c101bc50dcfed5689ff1
Priya-CP <priyabadiger37@gmail.com>
2026-08-25 11:50:47 +0530

    save this file

--- Name.py
+++ Name.py
@@ -1,4 +1,13 @@
-name = input("Enter your name:")
-surname = input("Enter your surname:")
+name = input("Enter your name: ")
 
-print ("your fullname is:",+surname)
+surname = input("Enter your surname: ")
+
+contact = input("Enter your contact number: ")
+
+city = input("Enter your city: ")
+
+print("Your fullname is:", name + " " + surname)
+
+print("Your contact is:", contact)
+
+print("Your city is:", city)
